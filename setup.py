41d2671ef9889c04eadff8afdd67526909b0ea6a
tushushu <m18591915960@163.com>
2021-09-21 21:24:39 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+Author: tushushu
+Date: 2021-09-21 21:11:42
+"""
+
+from setuptools import setup
+
+
+setup(name='funcy',
+      version='0.1.0',
+      description='Python library with chain functional programming.',
+      author='Tushushu',
+      url='https://github.com/tushushu/funcy',
+      packages=['funcy'],
+      )
